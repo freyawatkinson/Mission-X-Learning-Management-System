29a7ab8c5fe3238240906551816da4df1bb3112c
Kerri-Anne Palmer <kampalmer1985@gmail.com>
2021-05-13 18:05:12 +1200

    Lets see what happens here

--- src/components/SidebarCheckbox/SideBar.js
+++ src/components/SidebarCheckbox/SideBar.js
@@ -1,44 +1,101 @@
-import React, { useState } from "react";
+import react from "react";
 import "./SideBar.css";
 
-function StudentLibSideBar() {
-	const [checkbox] = useState([
-		{ title: "Subscription", itemTypes: ["Free", "Premium"], id: 1 },
-		{
-			title: "Activity Type",
-			itemTypes: ["Animation", "Game", "Chatbot", "Augmented Reality"],
-			id: 2,
-		},
-		{ title: "Year Level", itemTypes: ["1-4", "5-6", "7-8", "9-13"], id: 3 },
-		{
-			title: "Subject Matter",
-			itemTypes: [
-				"Computer Science",
-				"Maths",
-				"Science",
-				"Language",
-				"Art",
-				"Music",
-			],
-			id: 4,
-		},
-	]);
-
-	return (
-		<div>
-			{checkbox.map((checkbox) => (
-				<div className="StudentLibrarySideBar">
-					<h1 className="Checkbox-heads">{checkbox.title}</h1>
-					{checkbox.itemTypes.map((item) => (
-						<div>
-							<input type="checkbox" className="CheckBox" />
-							{item}
-						</div>
-					))}
-				</div>
-			))}
-		</div>
-	);
+function Sidebar() {
+  return (
+    <div className="SideBar">
+      <div className="SBContent">
+        <div>
+          <h3>SUBSCRIPTION</h3>
+          <label>
+            <input type="checkbox" />
+            Free
+          </label>
+          <br />
+          <label>
+            <input type="checkbox" />
+            Premium
+          </label>
+          <br />
+        </div>
+        <div>
+          <h3>ACTIVITY TYPE</h3>
+          <label>
+            <input type="checkbox" />
+            Animation
+          </label>
+          <br />
+          <label>
+            <input type="checkbox" />
+            Game
+          </label>
+          <br />
+          <label>
+            <input type="checkbox" />
+            Chatbot
+          </label>
+          <br />
+          <label>
+            <input type="checkbox" />
+            Augmented Reality
+          </label>
+          <br />
+        </div>
+        <div>
+          <h3>YEAR LEVEL</h3>
+          <label>
+            <input type="checkbox" />1 - 4
+          </label>
+          <br />
+          <label>
+            <input type="checkbox" />5 - 6
+          </label>
+          <br />
+          <label>
+            <input type="checkbox" />7 - 8
+          </label>
+          <br />
+          <label>
+            <input type="checkbox" />9 - 13
+          </label>
+          <br />
+        </div>
+        <div>
+          <h3>SUBJECT MATTER</h3>
+          <label>
+            <input type="checkbox" />
+            Computer Science
+          </label>
+          <br />
+          <label>
+            <input type="checkbox" />
+            Maths
+          </label>
+          <br />
+          <label>
+            <input type="checkbox" />
+            Science
+          </label>
+          <br />
+          <label>
+            <input type="checkbox" />
+            Language
+          </label>
+          <br />
+          <label>
+            <input type="checkbox" />
+            Art
+          </label>
+          <br />
+          <label>
+            <input type="checkbox" />
+            Music
+          </label>
+          <br />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default StudentLibSideBar;
+export default Sidebar;
--- src/components/StudentProjectLibrary/StudProjLib.js
+++ src/components/StudentProjectLibrary/StudProjLib.js
@@ -6,26 +6,26 @@ import Footer from "../AppFooter/AppFooter";
 import SPmaincontent from "./Main.ContentSP.js";
 
 function Projectlib() {
-	return (
-		<div>
-			<TopNavBar />
+  return (
+    <div>
+      <TopNavBar />
 
-			<div className="mainProjectContainer">
-				<div className="projectSideBar">
-					<ProjectSideBar />
-				</div>
-				<div className="projectContent">
-					<h1>Projects</h1>
-					<h4>
-						Welcome to the project library. You can use the filters on the right
-						to help you search for specific projects.
-					</h4>
-					<SPmaincontent />
-				</div>
-			</div>
-			<Footer />
-		</div>
-	);
+      <div className="mainProjectContainer">
+        <div className="projectSideBar">
+          <ProjectSideBar />
+        </div>
+        <div className="projectContent">
+          <h1>Projects</h1>
+          <h4>
+            Welcome to the project library. You can use the filters on the right
+            to help you search for specific projects.
+          </h4>
+          <SPmaincontent />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Projectlib;
